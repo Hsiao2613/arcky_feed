37c038c79b8af44700a6415c6b263ab3e8ee5ff7
Hsiao2613 <arctoson@gmail.com>
2025-06-19 20:20:00 +0800

    Add files via upload

---
+++ js.js
@@ -0,0 +1,16 @@
+﻿// FB icon 互動效果已在 CSS :hover 實現
+
+// 返回頂部按鈕
+const toTopBtn = document.getElementById("toTopBtn");
+
+window.onscroll = function() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        toTopBtn.style.display = "block";
+    } else {
+        toTopBtn.style.display = "none";
+    }
+};
+
+toTopBtn.addEventListener("click", () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
